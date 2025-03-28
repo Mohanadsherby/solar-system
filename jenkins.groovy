35421bce61312b15ee62fd63d24236c92e75196b
mohanad <mohanad@mohanad.com>
2025-03-28 05:34:24 +0200

    dep

---
+++ jenkins.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+
+    tools {
+        nodejs 'nodejs-22-6-0'
+    }
+
+    stages {
+        stage('Install Dependencies') {
+            steps {
+                script {
+                    // Install dependencies without auditing
+                    sh 'npm install --no-audit'
+                }
+            }
+        }
+
+        stage('NPM Dependency Audit') {
+            steps {
+                sh 'npm audit --audit-level=critical'
+
+                }
+            }
+        }
+    }    
